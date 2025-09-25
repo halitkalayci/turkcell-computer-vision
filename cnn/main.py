1b582624be4c0ec52d9b1f1be3e347680cd8598a
halitklyc <halitenes.kalayci@gmail.com>
2025-09-25 19:52:59 +0300

    Model eğitimine yeni geri çağırma fonksiyonları eklendi; erken durdurma süresi 6'ya çıkarıldı, en iyi model kaydedildi ve öğrenme oranı otomatik olarak düşürülecek şekilde ayarlandı. Eğitim süresi 100 epoch'a çıkarıldı. Ayrıca, rastgele görüntü tahminleri yapan yeni bir fonksiyon eklendi.

--- cnn/main.py
+++ cnn/main.py
@@ -31,7 +31,11 @@ def define_and_train_model():
 
     callbacks = [
         # düşüşten 3. epoch geçtikten sonra eğitimi durdur.
-        tf.keras.callbacks.EarlyStopping(patience=3, monitor="val_accuracy", restore_best_weights=True) 
+        tf.keras.callbacks.EarlyStopping(patience=6, monitor="val_accuracy", restore_best_weights=True),
+        # tüm epochlar içinden en iyi skora sahip olanı seç.
+        tf.keras.callbacks.ModelCheckpoint("best_model.keras", save_best_only=True),
+        # learning rate oranını otomatik düşür.
+        tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.3, patience=3, min_lr=0.0001)
     ]
 
     model = tf.keras.Sequential([
@@ -45,11 +49,13 @@ def define_and_train_model():
     ])
     model.summary()
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-    model.fit(X_train, y_train, epochs=5, validation_split=0.2, batch_size=25, callbacks=callbacks)
+    model.fit(X_train, y_train, epochs=100, validation_split=0.2, batch_size=25, callbacks=callbacks)
     model.save("mnist_model.keras")
 
+#define_and_train_model()
+
 def load_model():
-    model = tf.keras.models.load_model("mnist_model.keras")
+    model = tf.keras.models.load_model("best_model.keras")
     return model
 
 def confusion_matrix():
@@ -85,7 +91,26 @@ def predict(img_path):
     pred = np.argmax(probs)
     return pred,probs
 
+def predict_x_images(number):
+    model = load_model()
+    indices = np.random.choice(len(X_test), number, replace=False)
+
+    y_probs = model.predict(X_test[indices])
+    y_pred = np.argmax(y_probs, axis=1)
+    y_true = y_test[indices]
+
+    plt.figure(figsize=(15,10))
+
+    for i, indx in enumerate(indices):
+        plt.subplot(5,6, i+1)
+        plt.imshow(X_test[indx].reshape(28,28), cmap="gray")
+        plt.title(f"Tahmin: {y_pred[i]}, Gerçek: {y_true[i]}")
+        plt.axis("off")
+
+    plt.show()
+    
 #confusion_matrix()
-pred, probs = predict("image3.png")
-print(f"Tahmin: {pred}")
-print(f"Olasılıklar: {probs}")
+#pred, probs = predict("image2.png")
+#print(f"Tahmin: {pred}")
+#print(f"Olasılıklar: {probs}")
+predict_x_images(20)
