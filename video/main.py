2063e237efbe9cf2e3682808ce9d79295e0121ce
halitklyc <halitenes.kalayci@gmail.com>
2025-10-07 19:39:07 +0300

    fps ile video orj. hızında döngü sağlandı.

--- video/main.py
+++ video/main.py
@@ -33,18 +33,24 @@ def main():
     title = "VIDEO ISLEME GIRIS"
     cv2.namedWindow(title, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(title, 1000, 500)    
-    
+
+    fps = capture.get(cv2.CAP_PROP_FPS)
+    print("[INFO] FPS: ", fps)
+    delay = int(1000/fps) # 100ms
     while True:
         ok, frame = capture.read()
+        #capture.grab() # bir kare atla.
         if not ok:
             print("[INFO] Kaynak bitti veya kamera akışı kesildi.")
             break
         frame = resize_keep_aspect(frame)
-        
+
         display = frame.copy()
 
         cv2.imshow(title, display)
-        key = cv2.waitKey(1) & 0xFF
+
+        key = cv2.waitKey(delay) & 0xFF # 1ms arayla kullanıcı tuşa basmıyosa 1ms aralıkta bekle ve devam et.
+
         if key == ord("q") or key == 27:
             print("[INFO] Çıkış yapılıyor...")
             break
