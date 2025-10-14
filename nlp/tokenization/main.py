a8afcfdf3525dabaab13e5ce4d6c887e6dc3a612
halitklyc <halitenes.kalayci@gmail.com>
2025-10-14 18:49:51 +0300

    .

--- nlp/tokenization/main.py
+++ nlp/tokenization/main.py
@@ -31,7 +31,39 @@ words = ["kitaplar","gözlükçüler","koşuyordu","güzelleştiriyor","gidiyoru
 for word in words:
     print(f"{word} -> {tr_stemmer.stemWord(word)}")
 #
+print("************ Zemberek ************")
 
+from zemberek import TurkishMorphology
+# Stemming-2
+morphology = TurkishMorphology.create_with_defaults()
+
+for word in words: 
+    analysis = morphology.analyze(word)
+    # gidiyordu -> git #fiil + iyor#zaman-eki #du geçmiş zaman eki
+    if analysis:
+        result = analysis.analysis_results
+        print("Analizin tamamı: ", len(result))
+        if result:
+            best_analysis = result[0]
+            stem = best_analysis.get_stem()
+            print(f"{word} -> {stem}")
+            print(f"En iyi Analiz: {best_analysis}")
+            # A3pl - A3sg -> 
+
+
+
+for word in words: 
+    analysis = morphology.analyze(word)
+    # gidiyordu -> git #fiil + iyor#zaman-eki #du geçmiş zaman eki
+    if analysis:
+        result = analysis.analysis_results
+        print("Analizin tamamı: ", len(result))
+        if result:
+            best_analysis = result[0]
+            stem = best_analysis.get_stem()
+            print(f"{word} -> {stem}")
+            print(f"En iyi Analiz: {best_analysis}")
+            # A3pl - A3sg -> 
 
 # The goal of the task (Görevin amacı)
 # Duygu analizi -> noktalama işaretleri önemli, dolayısıyla noktalama işarelteri tek başına ayrı birer eleman olmalı.
