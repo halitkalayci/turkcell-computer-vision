a92f43e0c45e1eb6106f2e9a30a619ea8c4ace62
halitklyc <halitenes.kalayci@gmail.com>
2025-10-14 19:55:50 +0300

    tf-idf

--- nlp/vectorization/main.py
+++ nlp/vectorization/main.py
@@ -30,3 +30,43 @@ import pandas as pd
 
 df = pd.DataFrame(vector.toarray(), columns=column_names)
 print(df)
+
+
+print("-"*50)
+print("2. adım - TF-IDF")
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer2 = TfidfVectorizer()
+
+# fit => Corpusu analiz eder. Kelimelerden unique bir sözlük çıakrt.
+# fit => Her kelimenin IDF'ini hesaplar.
+# transform => Her bir cümleyi, her kelime için TF*IDF skorunu hesaplayarak vectorize eder.
+tfidf_matrix = vectorizer2.fit_transform(corpus)
+vocabulary2 = vectorizer2.vocabulary_
+print("Kelime listesi: ", vocabulary2)
+
+idf_scores = vectorizer2.idf_
+print("IDF skorları: ", idf_scores)
+
+df_idf = pd.DataFrame(idf_scores, index=vectorizer2.get_feature_names_out(), columns=["IDF"])
+print(df_idf)
+
+print("-"*50)
+
+df_tfidf = pd.DataFrame(tfidf_matrix.toarray(), columns=vectorizer2.get_feature_names_out())
+print(df_tfidf)
+
+corpus = [
+    "kahve sıcak ve güzel kahve", #mutlu
+    "çay da sıcak ve lezzetli", #üzgün
+    "kahve ve çay favori içeceklerim" #mutlu
+]
+
+# güzel
+# 1 belgede geçiyor df = 1
+# IDF = log((1+3) / (1+1)) + 1 = log(4/2) + 1 = log(2) + 1
+
+# ve
+# 3 belgede geçiyor
+# IDF = log((1+3) / (1+3)) + 1 = log(4/4) + 1 = log(1) + 1
