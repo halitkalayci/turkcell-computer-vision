1cf8f1b2bc0898ba3b43a74a9ab2121306883e29
halitklyc <halitenes.kalayci@gmail.com>
2025-10-21 20:18:51 +0300

    add rnn education

--- nlp/rnn/dataset.py
+++ nlp/rnn/dataset.py
@@ -3,14 +3,15 @@ from keras.preprocessing import sequence
 import random
 
 max_features= 10_000
+max_len = 200
 
 (X_train, y_train), (X_test,y_test) = imdb.load_data(num_words=max_features)
 
-X_train = sequence.pad_sequences(X_train, maxlen=200, padding="post")
-X_test = sequence.pad_sequences(X_test,maxlen=200, padding="post")
+X_train = sequence.pad_sequences(X_train, maxlen=max_len, padding="post")
+X_test = sequence.pad_sequences(X_test,maxlen=max_len, padding="post")
 
 print("Eğitim veri seti boyutu:", X_train.shape)
 print("Test veri seti boyutu:", X_test.shape)
 
-print(f"{len(X_train[20])}")
-print(f"Rastgele bir yorum {X_train[60]}  ....")
+#print(f"{len(X_train[20])}")
+#print(f"Rastgele bir yorum {X_train[60]}  ....")
--- nlp/rnn/main.py
+++ nlp/rnn/main.py
@@ -4,12 +4,17 @@
 from keras.models import Sequential
 from keras.layers import SimpleRNN, Dense, Embedding,Input
 
+from nlp.rnn.dataset import X_train, max_features, max_len, y_train
+
 
 model = Sequential()
-model.add(Input(shape=(10,))) # 10 kelime SimpleRNN için müthiş, büyüdükçe problem!
-model.add(Embedding( input_dim=10000, output_dim=64 )) # input_dim * output_dim = 64k
+model.add(Input(shape=(max_len,))) # 10 kelime SimpleRNN için müthiş, büyüdükçe problem!
+model.add(Embedding( input_dim=max_features, output_dim=64 )) # input_dim * output_dim = 64k
 model.add(SimpleRNN(64)) # "32 nöronlu hafızalı beyin" (input_size+hidden_size) * hidden_size + hidden_size (64+32) * 32+32
 model.add(Dense(1, activation="sigmoid")) # pozitif/negatif tahmini -> 32*1+1 => 33
 
 model.compile(optimizer="adam",loss="binary_crossentropy", metrics=["accuracy"])
+
+history_rnn = model.fit(X_train, y_train, epochs=2, batch_size=64, validation_split=0.2)
+
 model.summary()
